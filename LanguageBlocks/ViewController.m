f2f166c313ddb0d6219f8018f7eb45c27c95255d
tylacock <tylacock@gmail.com>
2013-10-18 18:43:19 -0400

    Inital Commit

--- LanguageBlocks/ViewController.m
+++ LanguageBlocks/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AVFoundation/AVSpeechSynthesis.h>
 
 @interface ViewController ()
 
@@ -24,6 +26,33 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)speak:(id)sender {
+    
+    NSString *code = [[NSString alloc] init];
+    
+    if ([sender viewWithTag:1]) {
+        code = @"en-us";
+    } else if ([sender viewWithTag:2]) {
+        code = @"es-MX";
+    } else if ([sender viewWithTag:3]) {
+        code = @"fr-CA";
+    } else {
+        code = @"en-AU";
+    }
+    
+    
+    
+    AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString: @"Where is the bathroom?"];
+    AVSpeechSynthesizer *synth = [[AVSpeechSynthesizer alloc] init];
+    
+    utterance.rate = AVSpeechUtteranceMinimumSpeechRate; // Speech Rate
+    utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage: code];
+    
+    [synth speakUtterance:utterance];
+
 }
 
 @end
---
+++ README.md
@@ -0,0 +1,3 @@
+###LanguageBlocks
+
+A simple app demo app that uses the new iOS7 speech synthesis to say phrases in different languages.
